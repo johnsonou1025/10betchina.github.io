18ec1aa8fc4b6cd636e564cb666323f648e6260a
johnson.o <johnson.o@10bet100.com>
2020-02-06 17:24:52 +0800

    add 10sport template

---
+++ 10sports/js/include.js
@@ -0,0 +1,12 @@
+$(function() {
+    $(".header").load("template.html .header>")
+    $(".nav").load("template.html .nav>")
+    $(".single-article").load("template.html .single-article>")
+    $(".single-article-n").load("template.html .single-article-n>")
+    $(".single-article-s").load("template.html .single-article-s>")
+    $(".single-article-l").load("template.html .single-article-l>")
+    $(".pager").load("template.html .pager")
+    $(".score-rank").load("template.html .score-rank>")
+    $(".footer").load("template.html .footer>")
+})
+
